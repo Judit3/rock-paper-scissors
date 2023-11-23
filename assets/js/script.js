8a25d14c2e47a5e46b2b41be211512da165ec298
Judit3 <>
2023-11-23 12:58:03 +0000

    start adding js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type")) {
+              let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+        }
+
+    runGame("xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+}); 
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after user's answer has been processed
+ */
+function runGame(gameType) {
+
+}
