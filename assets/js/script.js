0e83bb8bea98fa52f19c3495e9eae31c70663c12
Judit3 <>
2023-11-27 12:44:32 +0000

    start readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,19 @@
  * Declare constants for Dom elements
  * and possible choices
  */
-let buttons = document.getElementsByClassName("button");
-let playerScore = document.getElementById("player-score");
-let computerScore = document.getElementById("computer-score");
-let playerImage = document.getElementById("player-img");
-let computerImage = document.getElementById("computer-img");
-let choices = ["rock", "paper", "scissors"];
+const buttons = document.getElementsByClassName("button");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-img");
+const computerImage = document.getElementById("computer-img");
+const choices = ["rock", "paper", "scissors"];
+
 
 /**
  * Add eventlistener to all the buttons
  */
 for (let button of buttons) {
-    button.addEventListener("click", function(){
+    button.addEventListener("click", function () {
         let playerChoice = this.getAttribute("data-type");
         playGame(playerChoice);
     });
@@ -23,10 +24,12 @@ for (let button of buttons) {
  * Game function. Accepts the chosen value of the selected button
  */
 function playGame(playerChoice) {
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt = choices[playerChoice];
+    const indice = choices.indexOf(playerChoice);
 
-    let computerChoice = Math.floor(Math.random() * 3);
+    playerImage.src = `assets/images/${choices[indice]}.png`;
+    playerImage.alt = choices[indice];
+
+    const computerChoice = Math.floor(Math.random() * 3);
 
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
@@ -50,89 +53,90 @@ function winner(playerChoice, computerChoice) {
         alert("Win");
     } else {
         alert("Lose");
-    }        
-}
-
-/**
- * Update scores
- */
-function updateScore(result) {
-/**
- * Declare constants for Dom elements
- * and possible choices
- */
-let buttons = document.getElementsByClassName("button");
-let playerScore = document.getElementById("player-score");
-let computerScore = document.getElementById("computer-score");
-let playerImage = document.getElementById("player-img");
-let computerImage = document.getElementById("computer-img");
-let choices = ["rock", "paper", "scissors"];
-
-/**
- * Add eventlistener to all the buttons
- */
-for (let button of buttons) {
-    button.addEventListener("click", function(){
-        let playerChoice = this.getAttribute("data-type");
-        playGame(playerChoice);
-    });
-}
-
-/**
- * Game function. Accepts the chosen value of the selected button
- */
-function playGame(playerChoice) {
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt = choices[playerChoice];
-
-    let computerChoice = Math.floor(Math.random() * 3);
-
-    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
-    computerImage.alt = choices[computerChoice];
-
-    let result = winner(choices[computerChoice], choices[playerChoice]);
-
-    updateScore(result);
-}
-
-/**
- * Check who the winner is
- */
-function winner(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        alert("Tie");
-    } else if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "paper" && computerChoice === "rock") ||
-        (playerChoice === "scissors" && computerChoice === "paper")
-    ) {
-        alert("Win");
-    } else {
-        alert("Lose");
-    }        
-}
-
-/**
- * Update scores
- */
-function updateScore(result) {
-
-    let playerScoreValue = parseInt(playerScore.innerText);
-    let computerScoreValue = parseInt(computerScore.innerText); 
-
-    if (result === "Win") {
-        playerScoreValue++;
-    } else if (result === "Lose") {
-        computerScoreValue++
     }
 }
 
+/**
+ * Update scores
+ */
+function updateScore(result) {
+    /**
+     * Declare constants for Dom elements
+     * and possible choices
+     */
+    let buttons = document.getElementsByClassName("button");
+    let playerScore = document.getElementById("player-score");
+    let computerScore = document.getElementById("computer-score");
+    let playerImage = document.getElementById("player-img");
+    let computerImage = document.getElementById("computer-img");
+    let choices = ["rock", "paper", "scissors"];
+
+    /**
+     * Add eventlistener to all the buttons
+     */
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let playerChoice = this.getAttribute("data-type");
+            playGame(playerChoice);
+        });
+    }
+
+    /**
+     * Game function. Accepts the chosen value of the selected button
+     */
+    function playGame(playerChoice) {
+
+        playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+        playerImage.alt = choices[playerChoice];
+
+        let computerChoice = Math.floor(Math.random() * 3);
+
+        computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+        computerImage.alt = choices[computerChoice];
+
+        let result = winner(choices[computerChoice], choices[playerChoice]);
+
+        updateScore(result);
+    }
+
+    /**
+     * Check who the winner is
+     */
+    function winner(playerChoice, computerChoice) {
+        if (playerChoice === computerChoice) {
+            alert("Tie");
+        } else if (
+            (playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")
+        ) {
+            alert("Win");
+        } else {
+            alert("Lose");
+        }
+    }
+
+    /**
+     * Update scores
+     */
+    function updateScore(result) {
+
+        let playerScoreValue = parseInt(playerScore.innerText);
+        let computerScoreValue = parseInt(computerScore.innerText);
+
+        if (result === "Win") {
+            playerScoreValue++;
+        } else if (result === "Lose") {
+            computerScoreValue++;
+        }
+    }
+
     let playerScoreValue = parseInt(playerScore.innerText);
-    let computerScoreValue = parseInt(computerScore.innerText); 
+    let computerScoreValue = parseInt(computerScore.innerText);
 
     if (result === "Win") {
         playerScoreValue++;
     } else if (result === "Lose") {
-        computerScoreValue++
+        computerScoreValue++;
     }
 }
